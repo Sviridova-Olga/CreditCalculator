443d87d6f385c09651e5efa116947b6e23165a10
SvOlga <sviridova-olga.iccf@yandex.ru>
2020-05-28 16:27:59 +0300

    Code modified

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        double creditAmount = 1_000_000;                 //Сумма кредита от 100_000 до 5_000_000
-        double percent = 9.99;                          //процентная ставка >0
-        int creditTimeYear = 1;                        //срок кредита в годах от 1 года до 3 лет
+        double creditAmount = 1_000_000;                           //Сумма кредита от 100_000 до 5_000_000
+        double percent = 9.99;                                    //процентная ставка >0
+        int[] creditTime = {12, 24, 36};                        //срок кредита в месяцах > 0
         CreditPaymentService service = new CreditPaymentService();
-        int totalPayment = (int)service.calculate(creditAmount, percent, creditTimeYear);
-        if (totalPayment != 0) {
-            System.out.println("Ежемесячный платеж = " + totalPayment);
-        } else {
-            System.out.println("Входные параметры не соответствуют условиям");
+
+        for (int i = 0; i < creditTime.length; i++) {
+            int time = creditTime[i];
+            int totalPayment = (int) service.calculate(creditAmount, percent, time);
+            if (totalPayment != 0) {
+                System.out.println("Ежемесячный платеж = " + totalPayment);
+            } else {
+                System.out.println("Входные параметры не соответствуют условиям");
+            }
+
         }
     }
 }
